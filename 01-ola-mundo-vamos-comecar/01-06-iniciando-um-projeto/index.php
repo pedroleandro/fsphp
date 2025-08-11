74c7716f458bda18e80e3555b691cb066fd1b295
Pedro Leandro Gomes da Silva <pedroleandrogomesdasilva@outlook.com>
2025-08-11 09:33:59 -0300

    first commit

---
+++ 01-ola-mundo-vamos-comecar/01-06-iniciando-um-projeto/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>FSPHP: Iniciando um Projeto</title>
+
+    <link rel="stylesheet" href="assets/style.css">
+
+</head>
+<body>
+<?php
+/**
+ * Bloco de PHP com variável start
+ * e comando de saída
+ */
+
+$start = "Vamos começar!";
+echo "<h1>Olá, Mundo! {$start}</h1>";
+echo "<p id='js'>Loading...</p>"
+?>
+
+<script src="assets/scripts.js"></script>
+</body>
+</html>
