d619c77e36041b99b2cacfe61492e78db90733b7
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2025-11-12 10:03:51 -0300

    feat: estrutura de controle

--- 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
+++ 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
@@ -9,6 +9,37 @@ fullStackPHPClassName("02.07 - Estruturas de controle");
  */
 fullStackPHPClassSession("if, elseif, else", __LINE__);
 
+dump("Olá, Mundo!");
+
+$test = false;
+
+if ($test) {
+    dump(true);
+} else {
+    dump(false);
+}
+
+$age = 28;
+
+if ($age < 20) {
+    dump("bandas coloridas");
+} elseif ($age > 20 && $age < 50) {
+    dump("ótimas bandas");
+} else {
+    dump("Rock roll raiz");
+}
+
+$hour = 3;
+
+if ($hour >= 5 && $hour < 23) {
+    if ($hour < 7) {
+        dump("Bob Marley");
+    } else {
+        dump("After Bridge");
+    }
+} else {
+    dump("ZzzzzzzZZZZ");
+}
 
 /*
  * [ isset ] https://php.net/manual/pt_BR/function.isset.php
@@ -16,12 +47,49 @@ fullStackPHPClassSession("if, elseif, else", __LINE__);
  */
 fullStackPHPClassSession("isset, empty, !", __LINE__);
 
+$rock = "";
+
+//verifica se existe
+if (isset($rock)) {
+    dump("Rock And Roll!");
+} else {
+    dump("Die");
+}
+
+$rockAndRoll = "AC/DC";
+
+//verifica se NÃO existe ou se É vazio
+if (empty($rockAndRoll)) {
+    dump("Rock não existe ou está vazio!");
+} else {
+    dump("Rock existe e toca {$rockAndRoll}");
+}
 
 /*
  * [ switch ] https://secure.php.net/manual/pt_BR/control-structures.switch.php
  */
 fullStackPHPClassSession("switch", __LINE__);
 
+$payment = "canceled";
 
+switch ($payment) {
+    case "billet_printed":
+        dump("Boleto Emitido!");
+        break;
+    case "canceled":
+        dump("Pagamento cancelado");
+        break;
+    case "past_due":
+    case "pending":
+        dump("Pagamento pendente!");
+        break;
+    case "approved":
+    case "completed":
+        dump("Pagamento aprovado!");
+        break;
+    default:
+        dump("Erro ao processar o pagamento!");
+        break;
+}
 
 
