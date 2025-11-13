5207e1ba764271e8eca3f86baca1febb1f661a66
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2025-11-13 14:17:12 -0300

    feat: estrutura de repetição

--- 02-iniciando-do-zero-com-php/02-08-estruturas-de-repeticao/index.php
+++ 02-iniciando-do-zero-com-php/02-08-estruturas-de-repeticao/index.php
@@ -8,11 +8,33 @@ fullStackPHPClassName("02.08 - Estruturas de repetição");
  */
 fullStackPHPClassSession("while, do while", __LINE__);
 
+$looping = 1;
+$while = [];
+
+while($looping <= 5){
+    $while[] = $looping;
+    $looping++;
+}
+
+dump($while);
+
+$looping = 5;
+$while = [];
+do{
+    $while[] = $looping;
+    $looping--;
+}while($looping >= 1);
+
+dump($while);
 
 /*
  * [ for ] https://php.net/manual/pt_BR/control-structures.for.php
  */
 fullStackPHPClassSession("for", __LINE__);
+
+for($loop = 1; $loop <= 5; $loop++){
+    echo "<p>{$loop}</p>";
+}
 
 
 /**
@@ -21,8 +43,34 @@ fullStackPHPClassSession("for", __LINE__);
  */
 fullStackPHPClassSession("break, continue", __LINE__);
 
+for($loop = 1; $loop <= 5; $loop++){
+    if($loop % 2 == 0){
+        continue;
+    }
+
+    if($loop > 7){
+        break;
+    }
+
+    echo "<p>Passou +2: {$loop}</p>";
+}
 
 /**
  * [ foreach ] https://php.net/manual/pt_BR/control-structures.foreach.php
  */
 fullStackPHPClassSession("foreach", __LINE__);
+
+$student = [
+    "name" => "Pedro",
+    "age" => 28,
+    "school" => "senac",
+    "isFlamenguista" => true
+];
+
+foreach ($student as $item){
+    echo "<p>{$item}</p>";
+}
+
+foreach ($student as $key => $value){
+    echo "<p>{$key}: {$value}</p>";
+}
