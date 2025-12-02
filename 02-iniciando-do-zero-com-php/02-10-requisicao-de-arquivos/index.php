7c656c5fda99505ed91a84b36dd3b87e532f2117
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2025-12-02 08:28:45 -0300

    feat: requisição de arquivos

--- 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/index.php
+++ 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/index.php
@@ -8,10 +8,32 @@ fullStackPHPClassName("02.10 - Requisição de arquivos");
  */
 fullStackPHPClassSession("include, include_once", __LINE__);
 
+//include "file.php";
+//echo "<p>Continue...</p>";
+
+include __DIR__ . "/header.php";
+
+$profile = new StdClass();
+$profile->name = "Pedro Leandro";
+$profile->company = "Senac";
+$profile->email = "pedro.silva@ma.senac.br";
+
+//var_dump($profile);
+
+include __DIR__ . "/profile.php";
 
 /*
  * [ require ] https://php.net/manual/pt_BR/function.require.php
  * [ require_once ] https://php.net/manual/pt_BR/function.require-once.php
  */
 fullStackPHPClassSession("require, require_once", __LINE__);
+
+//require "file.php";
+//echo "<p>Continue...</p>";
+
+require __DIR__ . "/config.php";
+require_once __DIR__ . "/config.php";
+echo "<h1>" . COURSE . "</h1>";
+
+
 
---
+++ 02-iniciando-do-zero-com-php/02-10-requisicao-de-arquivos/profile.php
@@ -0,0 +1,9 @@
+<article style="
+    padding: 5px 20px;
+    background: #eeeeee;
+    border-radius: 4px;
+">
+    <h1><?= $profile->name ?></h1>
+    <p>Trabalha no <?= $profile->company ?></p>
+    <a title="Contate-me" href="mailto:<?= $profile->email ?>">Contate-me</a>
+</article>
