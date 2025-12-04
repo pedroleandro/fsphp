16e5f83020ca0189df8ce7ff8ae11da09217ed44
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2025-12-03 23:05:25 -0300

    feat: constantes e constantes mágicas

--- 02-iniciando-do-zero-com-php/02-12-valores-constantes/index.php
+++ 02-iniciando-do-zero-com-php/02-12-valores-constantes/index.php
@@ -7,8 +7,31 @@ fullStackPHPClassName("02.12 - Constantes e constantes mágicas");
  */
 fullStackPHPClassSession("constantes", __LINE__);
 
+define("COURSE", "Técnico em Informática");
+const TEACHER = "Pedro Leandro";
+
+$informarion = true;
+
+if($informarion) {
+    define("COURSE_TYPE", "Formação");
+}else{
+    define("COURSE_TYPE", "Curso");
+}
+
+echo "<p>{COURSE_TYPE} {COURSE} {TEACHER}</p>";
+echo "<p>". COURSE_TYPE . " " . COURSE . " Por " . TEACHER . "</p>";
 
 /*
  * [ constantes mágicas ] https://php.net/manual/pt_BR/language.constants.predefined.php
  */
 fullStackPHPClassSession("constantes mágicas", __LINE__);
+
+var_dump(get_defined_constants(true)["user"]);
+
+var_dump(
+    [
+        __LINE__,
+        __FILE__,
+        __DIR__,
+    ]
+);
