a56129e19ff2e30fc1cf225b207e70cb103aee43
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2025-12-03 23:07:10 -0300

    feat: manipulação e tratamento

---
+++ 03-manipulação-e-tratamento/10-interaco-com-urls/index.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__ . '/../../fullstackphp/fsphp.php';
+fullStackPHPClassName("10 - Interação com URLs");
+
+/*
+ * [ argumentos ] ?[&[&][&]]
+ */
+fullStackPHPClassSession("argumentos", __LINE__);
+
+
+/*
+ * [ segurança ] get | strip | filters | validation
+ * [ filter list ] https://php.net/manual/en/filter.filters.php
+ */
+fullStackPHPClassSession("segurança", __LINE__);
