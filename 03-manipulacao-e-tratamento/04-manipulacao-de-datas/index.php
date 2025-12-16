8139ef4831f04fc5a8a81c97ff8e3accb2613d6c
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2025-12-15 23:32:39 -0300

    feat: manipulação de datas

--- 03-manipulacao-e-tratamento/04-manipulacao-de-datas/index.php
+++ 03-manipulacao-e-tratamento/04-manipulacao-de-datas/index.php
@@ -9,8 +9,42 @@ fullStackPHPClassName("04 - Manipulação de datas");
  */
 fullStackPHPClassSession("a função date", __LINE__);
 
+var_dump([
+    date_default_timezone_get(),
+    date(DATE_W3C),
+    date("d/m/Y H:i:s")
+]);
+
+define("DATE_BR", "d/m/Y H:i:s");
+//define("DATE_TIMEZONE", "Pacific/Apia");
+define("DATE_TIMEZONE", "America/Sao_Paulo");
+
+date_default_timezone_set(DATE_TIMEZONE);
+
+var_dump([
+    date_default_timezone_get(),
+    date(DATE_BR),
+]);
+
+var_dump(getdate());
+
+echo "<p>Hoje é dia ", getdate()['mday'] ,"!</p>";
 
 /**
  * [ string to date ] strtotime | strftime
  */
 fullStackPHPClassSession("string to date", __LINE__);
+
+var_dump([
+    "strtotime_NOW" => strtotime("now"),
+    "time" => time(),
+    "strtotime+10d" => strtotime("+10days"),
+    "date current" => date(DATE_BR),
+    "date + 10days" => date(DATE_BR, strtotime("+10days")),
+    "date - 10days" => date(DATE_BR, strtotime("-10days")),
+    "date + 1years" => date(DATE_BR, strtotime("+1years")),
+]);
+
+$format = "%d/%m/%Y %Hh:%M";
+echo "<p>A data de hoje é ", strftime($format) ,"!</p>";
+echo strftime("Hoje é dia %d de %m de %Y ás %H horas e %m minutos");
