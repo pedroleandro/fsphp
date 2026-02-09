cab013a0af8cbd0b62f0e1cc740058e1e09ec258
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2026-02-09 09:46:15 -0300

    feat: Gestão de dependências

--- 07-recursos-e-componentes/07-03-gestao-de-dependencias/index.php
+++ 07-recursos-e-componentes/07-03-gestao-de-dependencias/index.php
@@ -1,4 +1,7 @@
 <?php
+
+use Source\Models\UserModel;
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("07.03 - Gestão de dependências");
 
@@ -7,3 +10,14 @@ fullStackPHPClassName("07.03 - Gestão de dependências");
  */
 fullStackPHPClassSession("get composer", __LINE__);
 
+require __DIR__ . "/../vendor/autoload.php";
+
+var_dump(
+    get_defined_constants(true)['user']
+);
+
+$user = (new UserModel())->findById(1);
+var_dump($user);
+
+$user = user()->findById(2);
+var_dump($user);
