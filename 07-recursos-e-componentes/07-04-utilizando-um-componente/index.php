bf2e45f6d92acfdfcd48c269b4dae553bd5debca
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2026-02-10 08:52:23 -0300

    Refactor: remove dados sensiveis e usa variaveis de ambiente

--- 07-recursos-e-componentes/07-04-utilizando-um-componente/index.php
+++ 07-recursos-e-componentes/07-04-utilizando-um-componente/index.php
@@ -34,7 +34,7 @@ try{
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.sendgrid.net';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = $_ENV['apikey'];                     //SMTP username
+    $mail->Username   = $_ENV['username'];                     //SMTP username
     $mail->Password   = $_ENV['password'];                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
