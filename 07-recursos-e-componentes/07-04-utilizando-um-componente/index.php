7bb666e0f068d90491b22218561226ad3d860beb
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2026-02-10 08:41:04 -0300

    feat: Utilizando um componente

--- 07-recursos-e-componentes/07-04-utilizando-um-componente/index.php
+++ 07-recursos-e-componentes/07-04-utilizando-um-componente/index.php
@@ -9,8 +9,54 @@ require __DIR__ . "/../vendor/autoload.php";
  */
 fullStackPHPClassSession("instance", __LINE__);
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception as MailException;
+use PHPMailer\PHPMailer\SMTP;
+
+$phpMailer = new PHPMailer();
+var_dump($phpMailer instanceof PHPMailer);
 
 /*
  * [ configure ]
  */
 fullStackPHPClassSession("configure", __LINE__);
+
+try{
+
+    $mail = new PHPMailer(true);
+
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.sendgrid.net';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'apikey';                     //SMTP username
+    $mail->Password   = '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';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->setLanguage('br');
+    $mail->CharSet = PHPMailer::CHARSET_UTF8;
+
+    //Recipients
+    $mail->setFrom('pedroleandrogomesdasilva@outlook.com', 'Pedro Silva');
+    $mail->addAddress('pedro.leandrog@gmail.com', 'Pedro Leandro');     //Add a recipient
+//    $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addReplyTo('pedroleandrogomesdasilva@outlook.com', 'Pedro Silva');
+//    $mail->addCC('cc@example.com');
+//    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+//    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+//    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Este é um exemplo de e-mail';
+    $mail->Body    = 'Este é um email <strong>com html!</strong>';
+    $mail->AltBody = 'Onde isso irá aparecer!?';
+
+    $mail->send();
+    echo 'Mensagem enviada com sucesso!';
+
+}catch(MailException $mailException){
+    var_dump($mailException);
+}
