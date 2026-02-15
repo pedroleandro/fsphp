33ab130f1e9b06606f8dddd72425d7e710d34e17
pedroleandro <pedroleandrogomesdasilva@outlook.com>
2026-02-15 13:27:12 -0300

    feat: Imagem, cache e miniaturas

--- 07-recursos-e-componentes/07-08-imagem-cache-e-miniaturas/index.php
+++ 07-recursos-e-componentes/07-08-imagem-cache-e-miniaturas/index.php
@@ -9,8 +9,18 @@ require __DIR__ . "/../vendor/autoload.php";
  */
 fullStackPHPClassSession("cropper", __LINE__);
 
+$thumb = new \Source\Core\Thumb();
+
+var_dump($thumb);
 
 /*
  * [ generate ]
  */
 fullStackPHPClassSession("generate", __LINE__);
+
+echo "<img src='{$thumb->make("images/2026/02/pedro.jpg", 300)}' alt='' title='' />";
+echo "<img src='{$thumb->make("images/2026/02/pedro.jpg", 180, 180)}' alt='' title='' />";
+
+var_dump($thumb->make("image.png", 200));
+
+$thumb->flush("images/2026/02/pedro.jpg");
